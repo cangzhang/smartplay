6d56d78f52fe979951b4ad3cdb969a9e1f86742c
cangzhang <cangzan@gmail.com>
2025-10-13 21:32:59 +0800

    feat(tests): enhance main test with improved navigation and selection logic
    * Update page navigation to wait for DOM content loaded
    * Implement logic to uncheck selected tags before selecting available items
    * Add functionality to select two consecutive available items for booking
    * Clean up commented-out code for better readability

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -9,13 +9,15 @@ const { USERNAME, PASSWORD } = process.env;
 // 每周一早上7点放号
 
 test('main', async ({ page }) => {
-  await page.setViewportSize({ width: 1920, height: 1080 });
+  // await page.setViewportSize({ width: 1600, height:  });
   await page.addInitScript(() => {
     const dt = new Date().getFullYear() + new Date().getHours();
     localStorage.setItem('devtools', `${dt}`);
     localStorage.setItem('webapplanguage', 'zh-cn');
   });
-  await page.goto('https://www.smartplay.lcsd.gov.hk/home');
+  // dont wait until network is idle, just wait until domcontentloaded
+  await page.goto('https://www.smartplay.lcsd.gov.hk/home', { waitUntil: 'domcontentloaded' });
+
   await expect(page.getByRole('heading', { name: '登入 SmartPLAY' })).toBeVisible();
   await page.getByRole('textbox', { name: 'SmartPLAY用户帐号或别名' }).fill(USERNAME!);
   await page.getByRole('textbox', { name: '密码' }).fill(PASSWORD!);
@@ -25,10 +27,43 @@ test('main', async ({ page }) => {
   const sunday = dayjs().day(0).add(7, 'day').format('YYYY-MM-DD');
 
   // await page.goto('https://www.smartplay.lcsd.gov.hk/facilities/search-result?keywords=&district=CW,EN,SN,WCH&startDate=&typeCode=DNRM&venueCode=&sportCode=DAAC&typeName=%E8%88%9E%E8%B9%88&frmFilterType=&venueSportCode=&isFree=false');
-  await page.goto(`https://www.smartplay.lcsd.gov.hk/facilities/select/court?venueId=207&fatId=311&venueName=%E7%9F%B3%E5%A1%98%E5%92%80%E4%BD%93%E8%82%B2%E9%A6%86&sessionIndex=0&dateIndex=0&playDate=${sunday}&district=CW,EN,SN,WCH&typeCode=DNRM&keywords=&sportCode=DAAC&frmFilterType=&isFree=false`);
-
+  await page.goto(`https://www.smartplay.lcsd.gov.hk/facilities/select/court?venueId=207&fatId=311&venueName=%E7%9F%B3%E5%A1%98%E5%92%80%E4%BD%93%E8%82%B2%E9%A6%86&sessionIndex=0&dateIndex=0&playDate=${sunday}&district=CW,EN,SN,WCH&typeCode=DNRM&keywords=&sportCode=DAAC&frmFilterType=&isFree=false`, {
+    waitUntil: 'domcontentloaded'
+  });
   await expect(page.getByText('上午段节')).toBeVisible({ timeout: 60_000 });
 
+  // uncheck all selected, classname 'session-tag-box-select'
+  const selectedTags = await page.$$('.session-tag-box-select');
+  for (const selected of selectedTags) {
+    await selected.click();
+  }
+
+  // find divs with .facilities-sc-content-all-item, from 10th to 13rd 
+  // if any two consecutive items, which contains text '可供租订', select them 
+  const items = await page.$$('.facilities-sc-content-all-item');
+  let selected = false;
+  for (let i = 9; i < 13; i++) {
+    const item = items[i];
+    const nextItem = items[i + 1];
+    const itemText = await item?.textContent() || '';
+    const nextItemText = await nextItem?.textContent() || '';
+    if (itemText.includes('可供租订') && nextItemText.includes('可供租订')) {
+      await item?.click();
+      await nextItem?.click();
+      if (item && nextItem) {
+        selected = true;
+      }
+      break;
+    }
+  }
+  console.log('selected', selected);
+  // proceed to confirm
+  await page.getByRole('button', { name: '继续' }).click();
+  // no for booking other instruments
+  await page.getByRole('button', { name: '否', exact: true }).click();
+  // confirm to payment
+  await page.getByRole('button', { name: '继续', exact: true }).click();
+
   const tokenHeader = await page.evaluate(() => localStorage.getItem('webappaccessToken'));
-  console.log('tokenHeader', tokenHeader);
+  // console.log('tokenHeader', tokenHeader);
 });
